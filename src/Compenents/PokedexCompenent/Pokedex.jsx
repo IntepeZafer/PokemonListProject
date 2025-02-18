f8cd7e3d97d41109737b817ec373f5a203a7e0d2
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-02-18 12:27:10 +0300

    first commit

---
+++ src/Compenents/PokedexCompenent/Pokedex.jsx
@@ -0,0 +1,14 @@
+import React , {useState , useEffect} from 'react'
+import axios from 'axios'
+
+
+const Pokedex = () => {
+    const [first, setfirst] = useState(second)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pokedex
